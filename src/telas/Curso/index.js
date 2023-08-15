35d3afd6d5359f99dcef80359c919b058f6b9337
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-08-15 09:30:30 -0300

    criando textos basicos

---
+++ src/telas/Curso/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Curso() {
+  return (
+    <View>
+      <Text>Curso</Text>
+    </View>
+  )
+}
---
+++ src/telas/Exercicio/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Exercicio() {
+  return (
+    <View>
+      <Text>Exercicio</Text>
+    </View>
+  )
+}
