b3d1bd12408794ec12c8061ec1e70f54159a2d26
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-08-15 09:26:06 -0300

    criando textos basicos

---
+++ src/mocks/textos.js
@@ -0,0 +1,45 @@
+const usuario = {
+    id: 1,
+    nome: "Isabela Mendes",
+    idade: "21 anos",
+    genero: "Feminino",
+    anoIngresso: "2022",
+    cpf: "31981894039",
+    curso: "DSM",
+    periodo: "manhã",
+    data_nascimento: "14/03/2003",
+    email: "isabela.mendes@fatec.sp.gov.br",
+    password: "@Abc123",
+    ra: 100100200,
+    username: "@isamendes"
+}
+
+const curso = {
+    cursoId: 1,
+    titulo: "Introdução ao Typescript",
+    descricao: 
+        "O curso de Typescript para iniciantes tem como objetivo proporcionar aos " +
+        "estudantes uma compreensão clara e profunda dos conceitos fundamentais " + 
+        "dessa linguagem de programação. Durante o curso, serão abordados desde " +
+        "os conceitos básicos, como tipos de dados, interfaces, classes e funções, "+
+        "até tópicos mais avançados, como generics, decorators e namespaces.",
+    cargaHoraria: "20 horas",
+    dataAtualizacao: "15/08/2023",
+    mediaAvaliacao: "0",
+    categoria: "Frontend",
+}
+
+const aula = {
+    aulaId: 1,
+    titulo: "Introdução",
+    video: "https://youtu.be/mRixno_uE2o",
+    descricao: "O que é Typescript, quais suas origens e motivações.",
+    conteudo: "O Typescript surgiu em ...",
+}
+
+const comentario = {
+    comentarioId: 1,
+    comentario: "Amei a aula",
+    username: "@isamendes",
+    nome: "Isabela Mendes"
+}
