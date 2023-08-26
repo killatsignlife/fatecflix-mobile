902ef7be788c42e844ab5ffb01b58082c009c587
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-08-26 20:06:07 -0300

    icones da tab bar

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import AppRotas from './src/rotas/AppRotas';
 export default function App() {
   return (
       <SafeAreaView style={{flex: 1}}>
+        <StatusBar/>
         <AppRotas/>
-        <StatusBar style="auto" />
       </SafeAreaView>
   );
 }
--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -5,12 +5,32 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../telas/Home';
 import Curso from '../telas/Curso';
 import Perfil from '../telas/Perfil';
+import { Feather } from 'react-native-vector-icons'
+
 
 export default function AppRotas() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route })=>({
+          headerShown: true,
+          tabBarActiveTintColor: '#ff0534',
+          tabBarInactiveTintColor: '#c7c7c7',
+          tabBarIcon: ({ color })=>{
+            if(route.name === 'Home'){
+              return <Feather name="home" size={24} color={color} /> 
+            } else if(route.name === 'Curso'){
+              return <Feather name="play" size={24} color={color} /> 
+            } else {
+              return <Feather name="user" size={24} color={color} />
+            }
+          },
+          tabBarLabelStyle: {
+            fontSize: 13,
+        },
+        })}
+        >
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="Curso" component={Curso} />
             <Tab.Screen name="Perfil" component={Perfil} />
