02415918a5cf70d29ab0701f3969384a0ef1e7da
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-09-05 21:17:22 -0300

    style:perfil

--- src/telas/Perfil/index.js
+++ src/telas/Perfil/index.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
 import React from 'react'
 
-export default function Perfil() {
+const Perfil = () => {
+
   return (
-    <View>
-      <Text>Perfil</Text>
+    <View style={styles.container}>
+      <View style={styles.containerFoto}>
+        <Image style={styles.imagem} source={ require('../../../assets/imagens/vendramel.png') }/>
+      </View>
     </View>
   )
 }
+
+export default Perfil;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(242, 107, 107, 0.21)"
+  },
+  containerFoto: {
+    margin: 8,
+    alignItems: 'center',
+    justifyContent: "center",
+    height: 240,
+  },
+  imagem:{
+    width: 200,
+    height: 200,
+    borderRadius: 200/2
+  }
+})
