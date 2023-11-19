717bb960199ecbb0c61af665e818fe9d807fb4cf
Silas Leite <bigchill_silas@hotmail.com>
2023-11-19 16:28:42 -0300

    :sparkles: feat: Componente de Comentarios

---
+++ src/mocks/comentarios.js
@@ -0,0 +1,40 @@
+export const comentarios = [
+  { 
+    id: 1,
+    name: 'brynn',
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
+    comment: 'Dmais!'
+  },
+  {
+    id: 2,
+    name: 'thot leader',
+    avatar:
+      'https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb',
+    comment: 'D+!'
+  },
+  {
+    id: 3,
+    name: 'jsa',
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
+    comment: 'Dimais!'
+  },
+  {
+    id: 4,
+    name: 'talhaconcepts',
+    avatar: 'https://randomuser.me/api/portraits/men/4.jpg',
+    comment: 'Demais!'
+  },
+  {
+    id: 5,
+    name: 'andy vitale',
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
+    comment: '10 mais!'
+  },
+  {
+    id: 6,
+    name: 'katy friedson',
+    avatar:
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxMTc1MTYzM15BMl5BanBnXkFtZTgwNzI5NjMwOTE@._V1_UY256_CR16,0,172,256_AL_.jpg',
+    comment: '10+!'
+  },
+];
--- src/telas/Aula/componentes/ExibeComentarios.js
+++ src/telas/Aula/componentes/ExibeComentarios.js
@@ -1,17 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, Image } from 'react-native'
 import React from 'react'
+import { comentarios } from '../../../mocks/comentarios'
+import { Feather } from 'react-native-vector-icons'
+import noPhoto from '../../../../assets/imagens/noPhoto.jpg'
 
 const ExibeComentarios = () => {
+  function obterComentarios() {
+    return comentarios.map((comentario) => {
+      return (
+        <TouchableOpacity
+          key={comentario.id}
+          onPress={() => Alert.alert(`Usuario: ${comentario.name}  comentou: comentario.comment`)}
+        >
+          <View style={styles.comentario}>
+            <View style={styles.containerComentario}>
+              <Image src={comentario.avatar} defaultSource={noPhoto} style={styles.avatar}/>
+              <Text style={styles.textoItemTitulo}>{comentario.name}</Text>
+            </View>
+            <View style={styles.content}>
+              <Text style={styles.contentText}>{comentario.comment}</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      )
+    })
+  }
   return (
-    <View style={{ flex: 1}}>
-      <Text>ExibeComentarios</Text>
-      <Text>ExibeComentarios</Text>
-      <Text>ExibeComentarios</Text>
-      <Text>ExibeComentarios</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.textoTitulo}>Comentários</Text>
+        {obterComentarios()}
+      </ScrollView>
     </View>
   )
 }
 
 export default ExibeComentarios
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    minHeight: 200,
+    paddingHorizontal: 8,
+  },
+  textoTitulo: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderWidth: 10,
+  },
+  comentario: {
+    flex: 1,
+    flexDirection: "column",
+    margin: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ECECEC",
+  },
+  content: {
+    margin: 8,
+  },
+  contentText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: 'gray'
+  },
+  containerComentario: {
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 8,
+    marginVertical: 4,
+  },
+  textoItemTitulo: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+});
